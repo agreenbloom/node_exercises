a1e597f9c9530a49cd4309fcc91af629a8fd66e1
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-07-17 13:50:51 -0400

    completed exercise 2

--- program.js
+++ program.js
@@ -1,1 +1,12 @@
-console.log("HELLO WORLD")
+// Exercise 1 - output "Hello World"
+// console.log("HELLO WORLD")
+
+
+// Exercise 2
+
+var result = 0;
+for (i = 2; i < process.argv.length; i++) {
+  result += Number(process.argv[i]);
+}
+
+console.log(result);
