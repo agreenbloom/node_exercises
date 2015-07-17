670002d19f6e94b9007bbe36a8039411a87cd8ba
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-07-17 14:19:56 -0400

    completed exercise 3

--- program.js
+++ program.js
@@ -4,9 +4,18 @@
 
 // Exercise 2
 
-var result = 0;
-for (i = 2; i < process.argv.length; i++) {
-  result += Number(process.argv[i]);
-}
+// var result = 0;
+// for (i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i]);
+// }
 
-console.log(result);
+// console.log(result);
+
+// Exercise 3 - My First I/O
+var fs = require('fs')
+
+var buf = fs.readFileSync('test')
+
+var str = buf.toString()
+
+console.log(str.split('\n').length - 1);
